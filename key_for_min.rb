6d863ca0c4d4511ed0163f739eaaabe1f94a2976
Berel Levy <berellevy@gmail.com>
2020-05-21 15:54:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_key = ""
+  min_key = nil
   lowest_number = 100
   name_hash.each do |key, value|
     if lowest_number > value
